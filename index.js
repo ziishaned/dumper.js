a34cf6f49cd602d5c20592738057c4e7118a0c61
Zeeshan Ahmad <ziishaned@gmail.com>
2018-08-04 00:57:59 +0400

    Update

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 let dd = require('./lib/dd');
 
 const obj = {
-    'elements':
-        {
-            'verb':
-                {
-                    'label': 'verb',
-                    'id': 'verb',
-                    'type': 'text',
-                    'size': '50',
-                    'element': 'input'
-                },
-            'url':
-                {
-                    'label': 'url',
-                    'id': 'url',
-                    'type': 'text',
-                    'size': '50',
-                    'element': 'input'
-                },
-            'reqBody':
-                {
-                    'label': 'reqbody',
-                    'id': 'reqbody',
-                    'type': 'text',
-                    'size': '50',
-                    'element': 'input'
-                }
-        }
+    foo: 'foo',
+    bar: {
+        foo: true,
+        bar: null,
+        baz: function () {
+            alert('Hello');
+        },
+    },
+    qux: [1.02, 3, 2, 'qwerty', 55],
+    quxx: /^[0-9]/,
 };
 
 dd(obj);
