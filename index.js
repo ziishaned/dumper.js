6393554aefc6049a7ad5ad987ad02df9b706a3c9
Zeeshan Ahmad <ziishaned@gmail.com>
2018-08-03 23:22:16 +0400

    Update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let dd = require('./lib/dd');
 
-dd();
+const obj = {
+    foo: 'foo',
+    bar: {
+        foo: true,
+        bar: null,
+        baz: function () {
+            alert('Hello');
+        },
+    },
+    qux: [1.02, 3, 2, 'qwerty', 55],
+    quxx: /^[0-9]/,
+};
+
+dd(obj);
