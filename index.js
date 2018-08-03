14809faca1cf421a4ce7a40c397ab069de8fb707
Zeeshan Ahmad <ziishaned@gmail.com>
2018-08-04 02:54:09 +0400

    Update

--- index.js
+++
@@ -1,9 +0,0 @@
-const dd = require('./lib/dd');
-
-const users = [
-    { user: 'barney', age: 36, active: true, createdAt: new Date(), getAge: () => this.age },
-    { user: 'fred', age: 40, active: false, createdAt: new Date(), getAge: () => this.age },
-    { user: 'pebbles', age: 1, active: true, createdAt: new Date(), getAge: () => this.age }
-];
-
-dd(users);
