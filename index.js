04a354fb480c586ca7b383083ee9da764e4977a6
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-02-10 12:10:46 +0700

    Reuse dump from dd

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const dd = require('./src/dd');
-const dump = require('./src/dump');
+const CallerId = require('caller-id');
+const Dumper   = require('./src/dumper');
+
+function dump(obj) {
+  const dumper = new Dumper();
+  let caller   = CallerId.getData();
+
+  if (caller.functionName === 'dd') {
+    caller = CallerId.getData(dd);
+  }
+
+  // Print the file path, line number and generated dump
+  console.log(`${caller.filePath}:${caller.lineNumber}:`);
+
+  console.log(dumper.generateDump(obj));
+}
+
+function dd(obj) {
+  dump(obj);
+
+  process.exit();
+}
 
 module.exports = {dd, dump};
